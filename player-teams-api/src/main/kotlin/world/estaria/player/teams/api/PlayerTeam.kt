16fdd6e4cf36f90ec588e24148c212ae0e4831f2
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-06-12 19:34:04 +0200

    add PlayerTeam

--- player-teams-api/src/main/kotlin/world/estaria/player/teams/api/PlayerTeam.kt
+++ player-teams-api/src/main/kotlin/world/estaria/player/teams/api/PlayerTeam.kt
@@ -6,6 +6,41 @@ package world.estaria.player.teams.api
 
 interface PlayerTeam {
 
+    /**
+     * @return true when this team a default group is
+     */
+    fun isDefault(): Boolean
 
+    /**
+     * Gets the team sorting string
+     * @return sorting string
+     */
+    fun getSorting(): String
+
+    /**
+     * Gets list of all present rank and sub ranks
+     * @return list of all ranks
+     */
+    fun getRanks(): List<String> {
+        return listOf(getPresentRank(), *getSubRanks().toTypedArray())
+    }
+
+    /**
+     * Gets the [TeamPatternType]
+     * @return string from pattern
+     */
+    fun getPattern(type: TeamPatternType): String
+
+    /**
+     * Gets the present rank of this team
+     * @return rank name
+     */
+    fun getPresentRank(): String
+
+    /**
+     * Gets list of all sub rank names
+     * @return list of all sub rank names
+     */
+    fun getSubRanks(): List<String>
 
 }
---
+++ player-teams-plugin/src/main/kotlin/world/estaria/player/teams/plugin/api/PlayerTeamImpl.kt
@@ -0,0 +1,32 @@
+package world.estaria.player.teams.plugin.api
+
+import world.estaria.player.teams.api.PlayerTeam
+import world.estaria.player.teams.api.TeamPatternType
+
+/**
+ * @author Niklas Nieberler
+ */
+
+class PlayerTeamImpl : PlayerTeam {
+
+    override fun isDefault(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSorting(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPattern(type: TeamPatternType): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPresentRank(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSubRanks(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+}
