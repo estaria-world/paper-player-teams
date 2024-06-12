be87cbb9e276dd901f22df952242398fafc7ac5f
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-06-12 19:31:03 +0200

    add TeamPatternType

---
+++ player-teams-api/src/main/kotlin/world/estaria/player/teams/api/TeamPatternType.kt
@@ -0,0 +1,17 @@
+package world.estaria.player.teams.api
+
+/**
+ * @author Niklas Nieberler
+ */
+
+enum class TeamPatternType {
+
+    CHAT,
+
+    TABLIST_PREFIX,
+
+    UNDER_PLAYER_PREFIX,
+
+    COLOR_CODE
+
+}
