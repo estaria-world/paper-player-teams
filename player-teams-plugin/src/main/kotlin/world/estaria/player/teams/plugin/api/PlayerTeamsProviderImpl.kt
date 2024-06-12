210e77aa0a300c162e751d217af0437446a453ab
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-06-12 19:39:20 +0200

    add PlayerTeamsProviderImpl implementation

--- player-teams-plugin/src/main/kotlin/world/estaria/player/teams/plugin/api/PlayerTeamsProviderImpl.kt
+++ player-teams-plugin/src/main/kotlin/world/estaria/player/teams/plugin/api/PlayerTeamsProviderImpl.kt
@@ -1,8 +1,11 @@
 package world.estaria.player.teams.plugin.api
 
 import org.bukkit.entity.Player
+import world.avionik.database.simplified.morphia.createMorphiaDatastore
 import world.estaria.player.teams.api.PlayerTeam
 import world.estaria.player.teams.api.PlayerTeamsProvider
+import world.estaria.player.teams.plugin.repository.PlayerTeamEntity
+import world.estaria.player.teams.plugin.repository.PlayerTeamRepository
 
 /**
  * @author Niklas Nieberler
@@ -10,16 +13,32 @@ import world.estaria.player.teams.api.PlayerTeamsProvider
 
 class PlayerTeamsProviderImpl : PlayerTeamsProvider {
 
+    private val repository = PlayerTeamRepository(createMorphiaDatastore("player_teams"))
+    private val playerTeamEntities = arrayListOf<PlayerTeamEntity>()
+
+    init {
+        updateTeamsCache()
+    }
+
     override fun updateTeamsCache() {
-        TODO("Not yet implemented")
+        this.playerTeamEntities.clear()
+        this.repository.findAll().thenAccept {
+            this.playerTeamEntities.addAll(it)
+        }
     }
 
     override fun getPlayerTeam(player: Player): PlayerTeam {
-        TODO("Not yet implemented")
+        return getPlayerTeams()
+            .sortedBy { it.getSorting() }
+            .firstOrNull { getInheritedGroupByRanks(player, it.getRanks()) } ?: return getDefaultTeam()
+    }
+
+    private fun getInheritedGroupByRanks(player: Player, ranks: List<String>): Boolean {
+        return ranks.any { player.hasPermission("group.$it") }
     }
 
     override fun getPlayerTeams(): List<PlayerTeam> {
-        TODO("Not yet implemented")
+        return this.playerTeamEntities.map { PlayerTeamImpl(it) }
     }
 
 }
