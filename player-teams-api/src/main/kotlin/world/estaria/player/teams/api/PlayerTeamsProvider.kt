27618bc30682f3f63c02f00c9e5f70e5dad35d2b
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-06-12 19:30:48 +0200

    add PlayerTeamsProvider

--- player-teams-api/src/main/kotlin/world/estaria/player/teams/api/PlayerTeamsProvider.kt
+++ player-teams-api/src/main/kotlin/world/estaria/player/teams/api/PlayerTeamsProvider.kt
@@ -1,8 +1,45 @@
 package world.estaria.player.teams.api
 
+import org.bukkit.entity.Player
+
 /**
  * @author Niklas Nieberler
  */
 
 interface PlayerTeamsProvider {
+
+    /**
+     * Updates the cached player teams list
+     */
+    fun updateTeamsCache()
+
+    /**
+     * Gets a cached [PlayerTeam] by a team name
+     * @param name of the team
+     */
+    fun getPlayerTeam(name: String): PlayerTeam {
+        return getPlayerTeams().firstOrNull { it.getPresentRank() == name }
+            ?: getDefaultTeam()
+    }
+
+    /**
+     * Gets a cached [PlayerTeam] by a player
+     * @param player the player
+     */
+    fun getPlayerTeam(player: Player): PlayerTeam
+
+    /**
+     * Gets a cached default [PlayerTeam]
+     */
+    fun getDefaultTeam(): PlayerTeam {
+        return getPlayerTeams().firstOrNull { it.isDefault() }
+            ?: throw IllegalArgumentException("failed to find default team format")
+    }
+
+    /**
+     * Gets all player teams from a database
+     * @return cached list of all teams
+     */
+    fun getPlayerTeams(): List<PlayerTeam>
+
 }
---
+++ player-teams-plugin/src/main/kotlin/world/estaria/player/teams/plugin/api/PlayerTeamsProviderImpl.kt
@@ -0,0 +1,25 @@
+package world.estaria.player.teams.plugin.api
+
+import org.bukkit.entity.Player
+import world.estaria.player.teams.api.PlayerTeam
+import world.estaria.player.teams.api.PlayerTeamsProvider
+
+/**
+ * @author Niklas Nieberler
+ */
+
+class PlayerTeamsProviderImpl : PlayerTeamsProvider {
+
+    override fun updateTeamsCache() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPlayerTeam(player: Player): PlayerTeam {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPlayerTeams(): List<PlayerTeam> {
+        TODO("Not yet implemented")
+    }
+
+}
